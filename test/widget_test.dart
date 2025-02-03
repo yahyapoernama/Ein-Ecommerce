e199b0b632fe10a0cedaf2e99a8198f027924582
Yahya Poernama <yahyapoernama@gmail.com>
2025-02-03 13:40:48 +0700

    fix: Widget Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,6 +15,7 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp(
       isFirstLaunch: true,
+      isLoggedIn: false,
     ));
 
     // Verify that our counter starts at 0.
