00dfa109712be95777894bab0fe098b306cfaf42
Yahya Poernama <yahyapoernama@gmail.com>
2025-02-06 05:27:39 +0700

    style: Implement chat functionality with search feature and dynamic chat list

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -7,10 +7,55 @@ class ChatPage extends StatefulWidget {
   State<ChatPage> createState() => _ChatPageState();
 }
 
+class Chat {
+  final String name;
+  final String message;
+  final String time;
+
+  Chat({required this.name, required this.message, required this.time});
+}
+
 class _ChatPageState extends State<ChatPage> with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
-  
+
+  final TextEditingController _searchController = TextEditingController();
+  List<Chat> _chats = [];
+  List<Chat> _filteredChats = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadChats();
+  }
+
+  void _loadChats() async {
+    _chats = [
+      Chat(name: 'John Doe', message: 'Hai, gimana kabar?', time: '10:00'),
+      Chat(name: 'Jane Doe', message: 'Baik, terima kasih', time: '11:00'),
+      Chat(name: 'Bob Smith', message: 'Saya ingin tahu lebih banyak tentang produk ini', time: '12:00'),
+      Chat(name: 'Alice Johnson', message: 'Saya ingin memesan produk ini', time: '13:00'),
+      Chat(name: 'Michael Brown', message: 'Saya ingin tahu lebih banyak tentang promo ini oapakahhhhhhhhhhhhhhhhhhhh', time: '14:00'),
+      Chat(name: 'Chris Evans', message: 'Apa ada diskon?', time: '15:00'),
+      Chat(name: 'Emma Watson', message: 'Bagaimana cara pembeliannya?', time: '16:00'),
+      Chat(name: 'Will Smith', message: 'Apakah produk ini tersedia?', time: '17:00'),
+      Chat(name: 'Scarlett Johansson', message: 'Berapa lama pengiriman?', time: '18:00'),
+      Chat(name: 'Robert Downey Jr.', message: 'Apa metode pembayaran?', time: '19:00'),
+    ];
+    setState(() {
+      _filteredChats = _chats;
+    });
+  }
+
+  void _searchChats(String query) {
+    final filteredChats = _chats.where((chat) {
+      return chat.name.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+    setState(() {
+      _filteredChats = filteredChats;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -21,6 +66,7 @@ class _ChatPageState extends State<ChatPage> with AutomaticKeepAliveClientMixin 
       child: Scaffold(
         backgroundColor: Colors.white,
         body: CustomScrollView(
+          physics: const ClampingScrollPhysics(),
           slivers: [
             SliverList(
               delegate: SliverChildListDelegate(
@@ -61,7 +107,14 @@ class _ChatPageState extends State<ChatPage> with AutomaticKeepAliveClientMixin 
                     ],
                   ),
                   child: TextField(
+                    controller: _searchController,
                     decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
                       hintText: 'Search',
                       prefixIcon: const Icon(Icons.search),
                       border: OutlineInputBorder(
@@ -77,6 +130,12 @@ class _ChatPageState extends State<ChatPage> with AutomaticKeepAliveClientMixin 
                         ),
                       ),
                     ),
+                    onSubmitted: (query) {
+                      _searchChats(query);
+                    },
+                    onTapOutside: (event) {
+                      _searchChats(_searchController.text);
+                    },
                   ),
                 ),
               ),
@@ -89,6 +148,7 @@ class _ChatPageState extends State<ChatPage> with AutomaticKeepAliveClientMixin 
                   return ListTile(
                     leading: CircleAvatar(
                       radius: 30,
+                      backgroundColor: Colors.orange[600],
                       child: ClipOval(
                         child: Image.asset(
                           'assets/images/dashboard/shirt1.webp',
@@ -97,14 +157,22 @@ class _ChatPageState extends State<ChatPage> with AutomaticKeepAliveClientMixin 
                           height: 50, // Atur tinggi gambar
                         ),
                       ),
-                      backgroundColor: Colors.orange[600],
                     ),
-                    title: const Text('User Name'),
-                    subtitle: const Text('Message'),
-                    trailing: const Text('10:00'),
+                    title: Text(
+                      _filteredChats[index].name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      _filteredChats[index].message,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    trailing: Text(_filteredChats[index].time),
                   );
                 },
-                childCount: 10, // Jumlah item dalam daftar
+                childCount: _filteredChats.length,
               ),
             ),
           ],
